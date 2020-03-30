d6020f28f8c4fdc7be7f6b2df16600e721312703
Stanciu Dragoș Ioan <39739297+StanciuDragosIoan@users.noreply.github.com>
2020-03-31 02:56:21 +0300

    Add files via upload

--- apps/react-complete-guide/src/App.js
+++ apps/react-complete-guide/src/App.js
@@ -5,29 +5,44 @@ import Person from './Person/Person';
 class App extends Component {
   state = {
     persons: [
-      {
-        name: 'Max',
-        age: 28
-      },
+      { name: 'Max', age: 28 },
+      { name: 'Manu', age: 29 },
+      { name: 'Stephanie', age: 26 }
+    ],
+    otherState: 'some other value'
+  };
 
-      {
-        name: 'Manu',
-        age: 29
-      },
+  switchNameHandler = newName => {
+    // console.log('Was clicked!');
+    // DON'T DO THIS: this.state.persons[0].name = 'Maximilian';
+    this.setState({
+      persons: [
+        { name: newName, age: 28 },
+        { name: 'Manu', age: 29 },
+        { name: 'Stephanie', age: 26 }
+      ]
+    });
+  };
 
-      {
-        name: 'Stephanie',
-        age: 26
-      }
-    ]
+  nameChangeHandler = event => {
+    this.setState({
+      persons: [
+        { name: 'Max', age: 28 },
+        { name: event.target.value, age: 29 },
+        { name: 'Stephanie', age: 27 }
+      ]
+    });
   };
 
   render() {
     return (
       <div className="App">
-        <h1>Hi, I am a React App</h1>
-        <p>This is really working..</p>
-        <button>Switch Name</button>
+        <h1>Hi, I'm a React App</h1>
+        <p>This is really working!</p>
+        {/* return the actual function call through an anonymous arrow function */}
+        <button onClick={() => this.switchNameHandler('Maxmillian')}>
+          Switch Name
+        </button>
         <Person
           name={this.state.persons[0].name}
           age={this.state.persons[0].age}
@@ -35,8 +50,11 @@ class App extends Component {
         <Person
           name={this.state.persons[1].name}
           age={this.state.persons[1].age}
+          //pass data to swtichnameHandler with bind() (most recommended)
+          click={this.switchNameHandler.bind(this, 'Max')}
+          change={this.nameChangeHandler}
         >
-          My hobbies: Racing
+          My Hobbies: Racing
         </Person>
         <Person
           name={this.state.persons[2].name}
@@ -44,6 +62,7 @@ class App extends Component {
         />
       </div>
     );
+    // return React.createElement('div', {className: 'App'}, React.createElement('h1', null, 'Does this work now?'));
   }
 }
 
