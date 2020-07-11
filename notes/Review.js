151f275370cf8eabec8f0a600501a6836a6d938c
stanciudragosioan <stanciudragosioan92@gmail.com>
2020-07-11 14:45:05 +0300

    review started

--- notes/Review.js
+++ notes/Review.js
@@ -1,23 +1,13 @@
 /*
 
-    ## Review number: 1 (date start april 29 2020)
+    ## Review number: 1 
+    
+    covered till video 32
     
     
     
-    april 29 2020
     
-    covered videos: Sections 1 and 2;
-
-
-    May 2 2020
-
-    covered videos:  26-42
-
-
-
     
-    May 3 2020
-
-    covered videos:  44-47
+    
 
 */
