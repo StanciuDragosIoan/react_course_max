49552c33d3554b96a36897dcc359e028f1466fbb
stanciudragosioan <stanciudragosioan92@gmail.com>
2020-07-21 10:48:48 +0300

    test

--- apps/sandbox/s1/react-course-max/src/App.js
+++ apps/sandbox/s1/react-course-max/src/App.js
@@ -26,12 +26,33 @@ const App = (props) => {
 
   const [otherState, setOtherState] = useState("Some name..");
 
-  const switchNameHandler = () => {
+  const switchNameHandler = (newName) => {
     console.log("Btn clicked and event triggered");
     setPersonsState({
       persons: [
         {
-          name: "TEST_NAME",
+          name: newName,
+          age: 28,
+        },
+
+        {
+          name: "Rosy",
+          age: 27,
+        },
+
+        {
+          name: "Kathy",
+          age: 25,
+        },
+      ],
+    });
+  };
+
+  const nameChangedHandler = (e) => {
+    setPersonsState({
+      persons: [
+        {
+          name: e.target.value,
           age: 28,
         },
 
@@ -51,14 +72,18 @@ const App = (props) => {
   return (
     <div className="App">
       <h1>Hi, I am a react app</h1>
-      <button onClick={switchNameHandler}>Switch Names</button>
+      <button onClick={() => switchNameHandler("TEST_NAME2")}>
+        Switch Names
+      </button>
       <Person
         name={personsState.persons[0].name}
         age={personsState.persons[0].age}
+        changed={nameChangedHandler}
       />
       <Person
         name={personsState.persons[1].name}
         age={personsState.persons[1].age}
+        click={switchNameHandler.bind(this, "TEST_NAME")}
       >
         My hobbies: Racing
       </Person>
